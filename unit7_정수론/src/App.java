acfb51ed98f9e9461a5161cb209e57ed3e0800db
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-07-03 00:10:11 +0900

    UNIT7_P1456

---
+++ unit7_정수론/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+
+    //static P1929_소수구하기 a = new P1929_소수구하기();
+    static P1456_거의소수 a = new P1456_거의소수();
+
+    public static void main(String[] args) throws Exception {
+        a.out();
+    }
+}
