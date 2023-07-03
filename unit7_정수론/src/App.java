08d8763d30a54f67e1bba4dbc334ab20f378c530
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-07-04 01:23:03 +0900

    UNIT7_P1747

--- unit7_정수론/src/App.java
+++ unit7_정수론/src/App.java
@@ -1,7 +1,8 @@
 public class App {
 
     //static P1929_소수구하기 a = new P1929_소수구하기();
-    static P1456_거의소수 a = new P1456_거의소수();
+    //static P1456_거의소수 a = new P1456_거의소수();
+    static P1747_소수_팰린드롬 a = new P1747_소수_팰린드롬();
 
     public static void main(String[] args) throws Exception {
         a.out();
