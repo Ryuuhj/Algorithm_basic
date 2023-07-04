a4c1b5b1cbe18edcab8eb0dbfc7de6377b5ade8a
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-07-04 18:39:36 +0900

    UNIT7_P1016

--- unit7_정수론/src/App.java
+++ unit7_정수론/src/App.java
@@ -1,8 +1,13 @@
-public class App {
+import 소수_구하기.P1929_소수구하기;
+import 소수_구하기.P1456_거의소수;
+import 소수_구하기.P1747_소수_팰린드롬;
+import 소수_구하기.P1016_제곱ㄴㄴ수;
 
+public class App {
     //static P1929_소수구하기 a = new P1929_소수구하기();
     //static P1456_거의소수 a = new P1456_거의소수();
-    static P1747_소수_팰린드롬 a = new P1747_소수_팰린드롬();
+    //static P1747_소수_팰린드롬 a = new P1747_소수_팰린드롬();
+    static P1016_제곱ㄴㄴ수 a = new P1016_제곱ㄴㄴ수();
 
     public static void main(String[] args) throws Exception {
         a.out();
---
+++ unit7_정수론/src/소수_구하기/P1016_제곱ㄴㄴ수.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class P1016_제곱ㄴㄴ수 {
+    public static void out(){
+        Scanner sc = new Scanner( System.in);
+        long min = sc.nextLong();
+        long max = sc.nextLong();
+        boolean[] square = new boolean[(int)(max-min+1)];
+        int cnt = 0;
+
+        for(long i = 2; i <= Math.sqrt(max); i++){
+            long sn = i*i;
+            long start_idx = min/sn;
+            if(min%sn != 0){
+                start_idx++;
+            }
+            for(long j = start_idx; j<=max/sn;j++){
+             square[(int)((j * sn) - min)] = true; //제곱수의 배수를 true로
+            }
+        }
+        
+        for(boolean tf : square){
+            if(!tf) cnt++;
+        }
+        System.out.println(cnt);
+
+    }
+}
