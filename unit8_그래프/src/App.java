4fd193337fe9f06b8244ea371e8661c750abcadb
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-07-09 23:08:42 +0900

    UNIT8_P2251

--- unit8_그래프/src/App.java
+++ unit8_그래프/src/App.java
@@ -1,10 +1,15 @@
 import 그래프의_표현.P1325_효율적인해킹;
+import 그래프의_표현.P1707_이분그래프;
 import 그래프의_표현.P18352_특정거리의_도시찾기;
+import 그래프의_표현.P2251_물통;
 
 public class App {
 
     //static P18352_특정거리의_도시찾기 a = new P18352_특정거리의_도시찾기();
-    static P1325_효율적인해킹 a= new P1325_효율적인해킹();
+    //static P1325_효율적인해킹 a= new P1325_효율적인해킹();
+    // static P1707_이분그래프 a = new P1707_이분그래프();
+    static P2251_물통 a = new P2251_물통();
+    
     public static void main(String[] args) throws Exception {
         a.out();
     }
