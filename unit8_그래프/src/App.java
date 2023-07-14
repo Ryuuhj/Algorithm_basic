c955ed8616a6c8e2757c607dce081f42c8368c37
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-07-14 23:28:04 +0900

    UNIT8_P1948

--- unit8_그래프/src/App.java
+++ unit8_그래프/src/App.java
@@ -3,6 +3,7 @@ import 그래프의_표현.P1707_이분그래프;
 import 그래프의_표현.P18352_특정거리의_도시찾기;
 import 그래프의_표현.P2251_물통;
 import 위상정렬.P1516_게임개발;
+import 위상정렬.P1948_임계경로;
 import 위상정렬.P2252_줄세우기;
 import 유니온_파인드.P1043_거짓말;
 import 유니온_파인드.P1717_집합의표현;
@@ -18,7 +19,9 @@ public class App {
     // static P1976_여행가자 a = new P1976_여행가자();
     // static P1043_거짓말 a = new P1043_거짓말();
     //static P2252_줄세우기 a = new P2252_줄세우기();
-    static P1516_게임개발 a = new P1516_게임개발();
+    // static P1516_게임개발 a = new P1516_게임개발();
+    static P1948_임계경로 a = new P1948_임계경로();
+    
 
     public static void main(String[] args) throws Exception {
         a.out();
---
+++ unit8_그래프/src/위상정렬/P1948_임계경로.java
@@ -0,0 +1,89 @@
+package 위상정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class P1948_임계경로 {
+    public static void out() throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int m = Integer.parseInt(bf.readLine());
+        StringTokenizer st;
+
+        ArrayList<ArrayList<int[]>> adjList = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            adjList.add(new ArrayList<>());
+        }
+        ArrayList<ArrayList<int[]>> reverse = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            reverse.add(new ArrayList<>());
+        }
+        int accTime[] = new int[n+1];
+        int inDegree[] = new int[n+1];
+
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            adjList.get(a).add(new int[] {b, t});
+            reverse.get(b).add(new int[] {a, t});
+            inDegree[b]++;
+        }
+        //출발 도시, 도착 도시 입력
+        st = new StringTokenizer(bf.readLine());
+        int startCity = Integer.parseInt(st.nextToken());
+        int endCity = Integer.parseInt(st.nextToken());
+    
+        //정방향 위상정렬
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(startCity);
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            for(int[] nextNode : adjList.get(now)){
+                int next = nextNode[0];
+                int time = nextNode[1];
+                inDegree[next]--;
+                accTime[next] = Math.max(accTime[next], (accTime[now]+time));
+                if(inDegree[next]==0)
+                    queue.offer(next);
+            }
+        }
+        //역방향 위상정렬 전, 중복 카운팅 방지할 중복 체크 배열
+        boolean[][] counted = new boolean[n+1][n+1];
+        int cnt = 0;
+        
+        //역방향 위상정렬 -> 2번 답
+        queue.clear();
+        queue.offer(endCity);
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            for(int[] preNode : reverse.get(now)){
+                int previous = preNode[0];
+                int time = preNode[1];
+                if(((accTime[previous]+time) == accTime[now])&&!counted[now][previous]){
+                    cnt++;
+                    queue.offer(previous);
+                    counted[now][previous] = true;
+                    // if(!counted[now][previous]){
+                    //     queue.offer(previous);
+                    //     counted[now][previous] = true;
+                    // }
+                    
+                }
+            }
+        }
+
+        System.out.println(accTime[endCity]);
+        System.out.println(cnt);
+        
+
+        
+
+    }
+}
