8b18dc47b7dd66b85736f7bc92ffab836743b0b0
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-08-13 23:09:02 +0900

    UNIT10_P1947

--- unit10_combination/src/P1947_선물전달.java
+++ unit10_combination/src/P1947_선물전달.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class P1947_선물전달 {
+    static int N;
+    static final long MOD = 1000000000;
+    static long[] dp;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        dp = new long[1000001];
+        //dp 초기화
+        dp[1] = 0;
+        dp[2] = 1;
+        N = sc.nextInt();
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (i - 1) * (dp[i - 2] + dp[i - 1]) % MOD;
+        }
+        System.out.println(dp[N]);
+    }
 
 }
