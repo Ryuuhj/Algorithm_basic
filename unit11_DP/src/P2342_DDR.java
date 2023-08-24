9b421b30856481c4531e12eb038d14cdd9106d9a
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-08-24 23:55:15 +0900

    UNIT11_P2342

--- unit11_DP/src/P2342_DDR.java
+++ unit11_DP/src/P2342_DDR.java
@@ -1,85 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class P2342_DDR {
-    static int[][][] D;
-    static String[] input;
+    static int[][][] dp;
+    static ArrayList<Integer> steps;
     static int length;
-    static int[] arr;
+    static int[][] values = {
+            {0, 2, 2, 2, 2},
+            {2, 1, 3, 4, 3},
+            {2, 3, 1, 3, 4},
+            {2, 4, 3, 1, 3},
+            {2, 3, 4, 3, 1}
+    };
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        input = bf.readLine().split(" ");
-        length = input.length - 1;
-        D = new int[100001][5][5];
-        for (int i = 0; i <= length; i++) {
-            for (int j = 0; j < 5; j++) {
-                Arrays.fill(D[i][j], Integer.MAX_VALUE);
-            }
+        dp = new int[100001][5][5];
+        steps = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        while (true) {
+            int n = Integer.parseInt(st.nextToken());
+            if (n == 0)
+                break;
+            steps.add(n);
         }
-        arr = new int[length];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(input[i]);
-        }
+        length = steps.size(); //이동 횟수
+        int result = getDDR(0, 0, 0);
 
-
-        D[0][0][0] = 0;
-        int next = arr[0];
-        D[1][0][next] = 2;
-        doDDR(1, 0, next);
-        D[1][next][0] = 2;
-        doDDR(1, next, 0);
-
-        int result = Integer.MAX_VALUE;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                result = Math.min(result, D[length][i][j]);
-            }
-        }
         System.out.println(result);
 
     }
 
-    private static void doDDR(int i, int l, int r) {
-        if (i == length) {
-            return;
+    private static int getDDR(int now, int l, int r) {
+        if (now == length)
+            return 0; //끝까지 도달한 경우(초기) => 0부터 시작해나감
+        //그 이전 값 존재하면 리턴 (추가 힘 누적해서 계산해야 하므로)
+        if (dp[now][l][r] != 0) {
+            return dp[now][l][r];
         }
+        int next = steps.get(now); //다음 스텝
+        //왼발로 next 밟는 경우 vs 오른발로 next 밟는 경우 중 최소로 갱신
+        int leftV = getDDR(now + 1, next, r) + values[l][next];
+        int rightV = getDDR(now + 1, l, next) + values[r][next];
+        dp[now][l][r] = Math.min(leftV, rightV);
 
-        int next = arr[i];
-        if (r == next) {
-
-        }
-        else {
-            int addL = 0;
-
-            if (Math.abs(l - next) % 2 == 1) {
-                addL = 3;
-            } else if (Math.abs(l - next) % 2 == 0) {
-                addL = 4;
-            } else if (l == next) {
-                addL = 1;
-            }
-            D[i + 1][next][r] = Math.min(D[i+1][next][r], D[i][l][r] + addL);
-            doDDR(i + 1, next, r);
-        }
-        if (l == next) {
-
-        }
-        else {
-            int addR = 0;
-
-            if (Math.abs(r - next) % 2 == 1) {
-                addR = 3;
-            } else if (Math.abs(r - next) % 2 == 0) {
-                addR = 4;
-            } else if (r == next) {
-                addR = 1;
-            }
-            D[i + 1][l][next] = Math.min(D[i+1][l][next], D[i][l][r] + addR);
-            doDDR(i + 1, l, next);
-        }
-
+        return dp[now][l][r];
     }
+
+
 }
