235d4de242aa5b7932c4986c4bb7e5829f2140f9
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-09-08 17:03:08 +0900

    P11654

---
+++ basic/src/P11654_아스키코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class P11654_아스키코드 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char in = sc.next().charAt(0);
+        System.out.println((int) in);
+    }
+}
