0de139b48892cd9a6baa8957d4c58fe878400e1b
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-09-14 02:53:09 +0900

    P11653

--- basic/src/약수_배수_소수/P11653_소인수분해.java
+++ basic/src/약수_배수_소수/P11653_소인수분해.java
@@ -6,6 +6,7 @@ public class P11653_소인수분해 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+
         if (N == 1)
             return;
 
