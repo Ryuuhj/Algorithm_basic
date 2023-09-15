cc5d989c937436ffec85c0f82a8c68441042cf41
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-09-15 23:31:01 +0900

    P15894

---
+++ basic/src/기하_직사각형_삼각형/P15894_수학은_체육과목.java
@@ -0,0 +1,12 @@
+package 기하_직사각형_삼각형;
+
+import java.util.Scanner;
+
+public class P15894_수학은_체육과목 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long a = 4L * n;
+        System.out.println(a);
+    }
+}
