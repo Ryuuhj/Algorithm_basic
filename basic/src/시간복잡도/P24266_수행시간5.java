78b902a13be846a26c4b796adb46aca58a116c40
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-09-16 23:44:52 +0900

    P24266

---
+++ basic/src/시간복잡도/P24266_수행시간5.java
@@ -0,0 +1,12 @@
+package 시간복잡도;
+
+import java.util.Scanner;
+
+public class P24266_수행시간5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((long) n * n * n);
+        System.out.println(3);
+    }
+}
