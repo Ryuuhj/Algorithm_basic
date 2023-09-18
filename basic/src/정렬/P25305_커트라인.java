5be8666b8355e0363c87ebd4bb203609c855ea87
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-09-18 23:41:49 +0900

    P25305

---
+++ basic/src/정렬/P25305_커트라인.java
@@ -0,0 +1,24 @@
+package 정렬;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P25305_커트라인 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] score = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.sort(score);
+        bw.write(score[N - k]+"");
+        bw.close();
+    }
+}
