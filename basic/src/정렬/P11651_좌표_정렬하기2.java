67a70ac026a3c6bc6d6abfddeb5ec9a60205eea3
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-09-20 23:55:15 +0900

    P11651

---
+++ basic/src/정렬/P11651_좌표_정렬하기2.java
@@ -0,0 +1,44 @@
+package 정렬;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class P11651_좌표_정렬하기2 {
+    static StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[][] points = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            points[i][0] = Integer.parseInt(st.nextToken());
+            points[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(points, new Comparator<int[]>(){
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] > o2[1]) {
+                    return 1;
+                } else if (o1[1] < o2[1]) {
+                    return -1;
+                } else {
+                    if (o1[0] > o2[0]) {
+                        return 1;
+                    }
+                }
+                return -1;
+            }
+        });
+
+        for (int[] p : points) {
+            bw.write(p[0] + " " + p[1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
