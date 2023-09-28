6026123d525157dd4504c3be1b0a139f1d1efb1a
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-09-28 23:30:22 +0900

    P24723

---
+++ basic/src/조합론/P24723_녹색거탑.java
@@ -0,0 +1,10 @@
+package 조합론;
+
+import java.util.Scanner;
+
+public class P24723_녹색거탑 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int)Math.pow(2, sc.nextInt()));
+    }
+}
