15658c67e0d18301e857788840ef6f54e5f022a9
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-09-29 21:49:59 +0900

    P15439

---
+++ basic/src/조합론/P15439_베라의패션.java
@@ -0,0 +1,11 @@
+package 조합론;
+
+import java.util.Scanner;
+
+public class P15439_베라의패션 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N * (N - 1));
+    }
+}
