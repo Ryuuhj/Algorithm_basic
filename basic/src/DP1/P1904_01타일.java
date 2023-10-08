e3c4de32f855b57806b51b8c5c4e59b83110240f
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2023-10-08 23:42:43 +0900

    P1904

---
+++ basic/src/DP1/P1904_01타일.java
@@ -0,0 +1,22 @@
+package DP1;
+
+import java.util.Scanner;
+
+public class P1904_01타일 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] dp = new int[N + 1];
+
+        if (N <= 2) {
+            System.out.println(N);
+            System.exit(0);
+        }
+
+        dp[1] = 1; dp[2] = 2;
+        for (int i = 3; i <= N ; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+        }
+        System.out.println(dp[N]);
+    }
+}
